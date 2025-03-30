8e0db3a0f585e6751f4730196681f3237d07c036
Caio <caio.alves.nog@gmail.com>
2025-03-30 12:39:05 -0300

    Removendo informações sensíveis

--- config/settings.py
+++ config/settings.py
@@ -161,8 +161,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 465  # Se 587 não funcionar, tente 465 e ative SSL
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = "ifeastcompany@gmail.com"  # Seu e-mail
-EMAIL_HOST_PASSWORD = "nnho ddza zgdc yimo"  # Sua senha ou senha de aplicativo
+EMAIL_HOST_USER = ""  # Seu e-mail
+EMAIL_HOST_PASSWORD = ""  # Sua senha ou senha de aplicativo
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 BASE_URL = "http://localhost:8000"  # Ou a URL do seu domínio em produção
